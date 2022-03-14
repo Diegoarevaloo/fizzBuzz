1952884bbccd1450b8b43bbdfa41481b2a3971a5
diegoarevaloo <diegoarevaloo@hotmail.com>
2022-03-14 15:10:04 -0500

    Se codifica el fro que genera todos los numeros.js

--- fizz.js
+++ fizz.js
@@ -1,1 +1,6 @@
-alert("Dame empleo");
+alert("Dame empleo");/*Genera una alerta que indica que el archivo js esta siendo bien invocado*/  
+var numeros = 100;/*se crea la variable numero*/
+for (var i = 1; i <= 100; i++)/*Se genera el ciclo for desde i con valor 0 hasta 100, aumentando 1 cada interacion*/
+{
+    document.write(i + "<br>"); /*Comando en Js que nos escribe el resultado del ciclo for, se encadena un html para salto de linea en cada iteraccion*/
+}
