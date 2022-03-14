998bb3bcbb7416c25808c0ab1dc18c381a7f3b1b
diegoarevaloo <diegoarevaloo@hotmail.com>
2022-03-14 15:45:38 -0500

    Se configura variable para hacer comparacion logica y terminar codigo

--- fizz.js
+++ fizz.js
@@ -1,9 +1,9 @@
 alert("Dame empleo");/*Genera una alerta que indica que el archivo js esta siendo bien invocado*/  
 var numeros = 100;/*se crea la variable numero*/
-var divisible= false;
+var divisible= false; /*Se genera esta variable para indicar una comparacion donde me permita imprimir fizzbuzz en caso de dos condiciones verdaderas*/
 for (var i = 1; i <= 100; i++)/*Se genera el ciclo for desde i con valor 0 hasta 100, aumentando 1 cada interacion*/
 {
-    divisible = false;
+    divisible = false;/*se reinicia la variable a falso despues de cada ciclo
     if (i % 3 ==0)/*Se evalua si el modulo de la operacion es 0, para confirmar el resultado. */
         {
           document.write("Fizz");  
