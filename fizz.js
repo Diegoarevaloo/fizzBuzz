5465038860ad385cc9b55105714dfac7d92a00fc
diegoarevaloo <diegoarevaloo@hotmail.com>
2022-03-15 09:35:36 -0500

    Se crea funcion que valua condicional

--- fizz.js
+++ fizz.js
@@ -3,14 +3,14 @@ var numeros = 100;/*se crea la variable numero*/
 var divisible= false; /*Se genera esta variable para indicar una comparacion donde me permita imprimir fizzbuzz en caso de dos condiciones verdaderas*/
 for (var i = 1; i <= 100; i++)/*Se genera el ciclo for desde i con valor 0 hasta 100, aumentando 1 cada interacion*/
 {
-    divisible = false;/*se reinicia la variable a falso despues de cada ciclo
-    if (i % 3 ==0)/*Se evalua si el modulo de la operacion es 0, para confirmar el resultado. */
+    divisible = false;/*se reinicia la variable a falso despues de cada ciclo*/
+    if (esDivisible(i,5))/*Se evalua si el modulo de la operacion es 0, para confirmar el resultado. */
         {
           document.write("Fizz");  
           divisible = true;
         }
 
-    if (i % 5 == 0)
+    if (esDivisible(i,3))
         {
           document.write("Buzz");      
           divisible = true;  
@@ -24,3 +24,15 @@ for (var i = 1; i <= 100; i++)/*Se genera el ciclo for desde i con valor 0 hasta
         }
         document.write("<br />") /*se mueve el salto de linea al un punto en cumun para todo el Script */
 }
+
+function esDivisible(num,divisor) /*Se agrega funcion que evalua la condicional*/
+  {
+    if(num % divisor == 0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
